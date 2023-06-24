ba45a7f556b565c2cecbe2453f5d5386e1ef99d0
Meriam <132971614+msadio8@users.noreply.github.com>
2023-06-24 13:36:30 +1000

    add new codes

--- script.js
+++ script.js
@@ -26,37 +26,37 @@ $(document).ready(function () {
       $(this).addClass("past");
     }
   });
-
+    
   //TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   
-    $(".saveBtn").click(function (event) {
+    $(".saveBtn").on("click", function() {
 
-        var value = $(this).siblings(".time-block").val();
-        var time = $(this).parent().attr("id").split("-")[1];
-        localStorage.setItem(time,value);
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time,text);
     });
-
+    
    //Retrieve data from local storage //
-   $("#hour-09 .time-block").val(localStorage.getItem("09"));
-   $("#hour-10 .time-block").val(localStorage.getItem("10"));
-   $("#hour-11 .time-block").val(localStorage.getItem("11"));
-   $("#hour-12 .time-block").val(localStorage.getItem("12"));
-   $("#hour-13 .time-block").val(localStorage.getItem("13"));
-   $("#hour-14 .time-block").val(localStorage.getItem("14"));
-   $("#hour-15 .time-block").val(localStorage.getItem("15"));
-   $("#hour-16 .time-block").val(localStorage.getItem("16"));
-   $("#hour-17 .time-block").val(localStorage.getItem("17"));
-
-
+   $("#hour-09 .description").val(localStorage.getItem("09"));
+   $("#hour-10 .description").val(localStorage.getItem("10"));
+   $("#hour-11 .description").val(localStorage.getItem("11"));
+   $("#hour-12 .description").val(localStorage.getItem("12"));
+   $("#hour-13 .description").val(localStorage.getItem("13"));
+   $("#hour-14 .description").val(localStorage.getItem("14"));
+   $("#hour-15 .description").val(localStorage.getItem("15"));
+   $("#hour-16 .description").val(localStorage.getItem("16"));
+   $("#hour-17 .description").val(localStorage.getItem("17"));
+   
     //Clear button function for clearing content and local storage
      $("#clearFieldsBtn").click(function(event) {
         event.preventDefault;
         $("textArea").val("");
         localStorage.clear();
     });
-  
+    
+    
 });
 
 
